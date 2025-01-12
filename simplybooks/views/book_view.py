885204f91b877f060bc52cd503505853d4c79d35
Trevor Mustoe <trevormustoe0405@gmail.com>
2025-01-12 12:58:36 -0500

    Working Post for creating a new book

--- simplybooks/views/book_view.py
+++ simplybooks/views/book_view.py
@@ -4,6 +4,7 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
 from simplybooks.models import Book
+from simplybooks.models import Author
 
 
 class BookView(ViewSet):
@@ -42,11 +43,43 @@ class BookView(ViewSet):
         # http://localhost:8000/book?uid=1
         if uid is not None:
             book = book.filter(uid=uid)
-            #This is where we could add another statement that returns everthing else that is considereed "public since there is no UID present"
+            
+##############################################################################################################################################
+#This is where we could add another statement that returns everthing else that is considered public since there is no UID present
+#This would have to check if uid is set to None to return those books only. Then we can have a user set to public or private true or false if true it will display it to public if 
+            
+# We would first need to get all books of course
+# Then we need to set a boolean statement on our books models 
+# use an if statement to check if the boolean of public is set to false 
+# if so maybe we could we return all the books that are set to false as well as only from the UID.
+# if not, (set to true)---> (else), we would just return all books to the user and dont need to check for UID since its now public
+# This feels messy but could work
+##############################################################################################################################################
    
    
          #This is serializing the data and converting it to a json    
         serializer = BookSerializer(book, many=True)
+        return Response(serializer.data)
+    
+    def create(self, request):
+        """Handle POST operations
+
+        Returns
+            Response -- JSON serialized game instance
+        """
+        # Here I am 
+        author = Author.objects.get(id=request.data["author"])
+
+        book = Book.objects.create(
+            author=author,
+            uid=request.data["uid"],
+            title=request.data["title"],
+            image=request.data["image"],
+            price=request.data["price"],
+            sale=request.data["sale"],
+            description=request.data["description"],
+        )
+        serializer = BookSerializer(book)
         return Response(serializer.data)
         
 class BookSerializer(serializers.ModelSerializer):
